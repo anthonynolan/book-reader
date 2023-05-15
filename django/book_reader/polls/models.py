a818e145fd1ff8a979395ec43e8c1e5ab785afe3
Anthony Nolan <anthonynolan@gmail.com>
2023-05-15 07:47:55 +0100

    Adding django templates

--- django/book_reader/polls/models.py
+++ django/book_reader/polls/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from dataclasses import dataclass
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 
 
+# @dataclass
 class Thing(models.Model):
     thing_name = models.TextField(max_length=50)
+    created = models.DateTimeField(default=timezone.now())
+
+    def was_published_recently(self):
+        return self.created >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
-        return self.thing_name
+        return f"{self.id}, {self.thing_name}, {self.created}"
--- django/book_reader/polls/urls.py
+++ django/book_reader/polls/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("", views.add, name="add"),
+    path("<thing_id>/", views.detail, name="detail"),
 ]
