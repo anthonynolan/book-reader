5225542246aeb5e6566770e8203a5d868ee38654
Anthony Nolan <anthonynolan@gmail.com>
2023-05-16 07:26:56 +0100

    Added Book model

--- django/book_reader/polls/admin.py
+++ django/book_reader/polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Thing
+from .models import Thing, Book
 
 admin.site.register(Thing)
+admin.site.register(Book)
--- django/book_reader/polls/models.py
+++ django/book_reader/polls/models.py
@@ -1,18 +1,25 @@
 from django.db import models
-from dataclasses import dataclass
 import datetime
 from django.utils import timezone
 
 # Create your models here.
 
 
-# @dataclass
 class Thing(models.Model):
     thing_name = models.TextField(max_length=50)
-    created = models.DateTimeField(default=timezone.now())
+    created = models.DateTimeField(default=timezone.now)
 
     def was_published_recently(self):
         return self.created >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
         return f"{self.id}, {self.thing_name}, {self.created}"
+
+
+class Book(models.Model):
+    title = models.TextField(max_length=100)
+    created = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.title}"
