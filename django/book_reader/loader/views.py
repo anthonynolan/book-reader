bbdbf269edb6b7b9f80612e08ae1b45a918f5dc1
Anthony Nolan <anthonynolan@gmail.com>
2023-05-21 10:39:29 +0100

    Word counter

--- django/book_reader/loader/views.py
+++ django/book_reader/loader/views.py
@@ -1,9 +1,9 @@
 # Create your views here.
 
-
-from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Book
+import json
+import pdb
 
 
 def index(request):
@@ -13,15 +13,9 @@ def index(request):
 
 def book_detail(request, book_id):
     book = Book.objects.get(pk=book_id)
-    return render(request, "loader/book_detail.html", {"book": book})
-
-
-def generate_stats(request, book_id):
-    book = Book.objects.get(pk=book_id)
     stats = {}
-    if book.description:
-        stats["word_count"] = book.description.split()
-    import json
-
+    if book.content_file:
+        content = book.content_file.read()
+        stats["word_count"] = len(content.split())
     book.stats = json.dumps(stats)
-    return render(request, "loader/book_stats.html", {"book": book})
+    return render(request, "loader/book_detail.html", {"book": book})
